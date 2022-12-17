dc6e5aff5f08284fb95fd14b8d4a63cfa089fef5
Ning Li <lining04111223@gmail.com>
2022-12-16 17:26:34 -0800

    create,connect to mongoDB

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
+const MongoDbId = process.env.MongoDB_URL;
+mongoose
+  .connect(MongoDbId)
+  .then(() => {
+    console.log("Connected to MongoDB atlas.");
+  })
+  .catch((err) => {
+    console.log("Connected Failed.");
+    console.log(err);
+  });
+
+//middware
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("Hi");
+});
+
+app.listen(8080, () => {
+  console.log("server running on 8080.");
+});

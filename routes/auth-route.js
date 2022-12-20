dcc61999a4d1087df6acbb0e36642d82fe68ef51
Ning Li <lining04111223@gmail.com>
2022-12-19 22:41:58 -0800

    signup

--- routes/auth-route.js
+++ routes/auth-route.js
@@ -1,14 +1,40 @@
 const router = require("express").Router();
 const passport = require("passport");
+const bcrypt = require("bcrypt");
+const User = require("../models/user-model");
 
 router.get("/login", (req, res) => {
-  res.render("login");
+  res.render("login", { user: req.user });
+});
+
+router.get("/signup", (req, res) => {
+  res.render("signup", { user: req.user });
 });
 
 router.get("/logout", (req, res) => {
   req.logOut();
   res.redirect("/");
 });
+
+router.post("/signup", async (req, res) => {
+  let { name, email, password } = req.body;
+  //check database
+  const emailExist = await User.findOne({ email });
+  if (emailExist) return res.status(400).send("Email already exist.");
+  const hash = await bcrypt.hash(password, 10);
+  password = hash;
+  let newUser = new User({ name, email, password });
+  try {
+    const saveUser = await newUser.save();
+    res.status(200).send({
+      msg: "User saves.",
+      savedObj: saveUser,
+    });
+  } catch (err) {
+    res.status(400).send(err);
+  }
+});
+
 router.get(
   "/google",
   passport.authenticate("google", {
